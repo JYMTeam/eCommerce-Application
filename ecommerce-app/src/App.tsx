298b1d9acad081a33ce38e31618d1a3671277918
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-03 16:15:41 +0200

    feat: install prettier

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { MainPage } from './pages/MainPage';
-import { AuthPage } from './pages/AuthPage';
-import { Navigation } from './components/Navigation';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { MainPage } from "./pages/MainPage";
+import { AuthPage } from "./pages/AuthPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      {/* навигация для примера */}
-      <Navigation />
+<div className="App">
+  {/* навигация для примера */}
+  <Navigation />
 
-      {/* пути для примера */}
-      <Routes>
-        <Route path='/' element={ <MainPage /> }></Route>
-        <Route path='/auth' element={ <AuthPage /> }></Route>
-      </Routes>
+  {/* пути для примера */}
+  <Routes>
+    <Route path="/" element={<MainPage />}></Route>
+    <Route path="/auth" element={<AuthPage />}></Route>
+  </Routes>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+</div>
   );
 }
 
