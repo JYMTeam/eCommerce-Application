3c333c95fae7b4f151753c799ec5c066bf51ac28
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-03 16:18:10 +0200

    feat: add json prettier

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -8,31 +8,31 @@ import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
-<div className="App">
-  {/* навигация для примера */}
-  <Navigation />
+    <div className="App">
+      {/* навигация для примера */}
+      <Navigation />
 
-  {/* пути для примера */}
-  <Routes>
-    <Route path="/" element={<MainPage />}></Route>
-    <Route path="/auth" element={<AuthPage />}></Route>
-  </Routes>
+      {/* пути для примера */}
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/auth" element={<AuthPage />}></Route>
+      </Routes>
 
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-</div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
