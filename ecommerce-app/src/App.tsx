bc3e6235d1347b4cb36f401d5e187b1e4b477d58
Mariya Demy <Mariya.Demi373@gmail.com>
2023-08-06 17:18:48 +0300

    feat: add api, styles, services, constants folders (EC-22)

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./assets/logo.svg";
-import "./App.css";
+import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { LoginPage } from "./pages/AuthPage";
