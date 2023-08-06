6260ca04a3b4db522a82b0c75514f089073654fa
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-06 19:32:05 +0200

    fix: change app-file v6 for tests (EC-35)

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -5,7 +5,6 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { LoginPage } from "./pages/AuthPage";
 import { Navigation } from "./components/Navigation";
-import { Button } from "./components/Button";
 
 function App() {
   return (
@@ -34,7 +33,6 @@ function App() {
     Learn React
   </a>
 </header> */}
-        <Button />
       </div>
     </BrowserRouter>
   );
