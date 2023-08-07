71abdb0650477ce6ae34bce9b5f34573189adc56
Julia Khvatova <jkhvatova@gmail.com>
2023-08-07 12:26:52 +0300

    feat: add example nav variation (EC-36)

--- ecommerce-app/src/components/Navigation.tsx
+++ ecommerce-app/src/components/Navigation.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Stack, Button } from "@mui/material";
 
 export function Navigation() {
   return (
     <div>
-      Navigation
-      <Link to="auth">Auth</Link>
+      <Stack spacing={3} direction="row">
+        <Button variant="text">Bags</Button>
+        <Button variant="text">Backpacks</Button>
+        <Button variant="text">Suitcases</Button>
+        <Button variant="text">Wallets</Button>
+        {/* <Link to="auth">Auth</Link> */}
+        <Button component={Link} to="Auth" variant="contained" color="primary">
+          Auth
+        </Button>
+        <Button component={Link} to="Main" variant="contained" color="primary">
+          Main
+        </Button>
+      </Stack>
     </div>
   );
 }
