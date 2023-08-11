0f0ca51e95cbba6a837bf2e3cbb44365b81ca409
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-11 20:37:18 +0200

    fix: loginPage (EC-42)

--- ecommerce-app/src/pages/LoginPage.tsx
+++ ecommerce-app/src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { fetchUserLogin } from "../store/actions/userLoginActions";
-import { useAppDispatch, useAppSelector } from "../hooks/redux";
+import { useAppDispatch } from "../hooks/redux";
 import { UserAuthOptions } from "@commercetools/sdk-client-v2";
+import { LoginForm } from "../components/LoginForm/LoginForm";
+import Container from "@mui/material/Container";
 
 const existingUser: UserAuthOptions = {
   username: "johndoe@example.com",
@@ -11,20 +13,18 @@ const existingUser: UserAuthOptions = {
 export function LoginPage() {
   const dispatch = useAppDispatch();
 
-  //получаем данные из стейта
-  const { error, loading, loginData } = useAppSelector(
-    (state) => state.userLogin,
-  );
+  // const { error, loading, loginData } = useAppSelector(
+  //   (state) => state.userLogin,
+  // );
 
   useEffect(() => {
     dispatch(fetchUserLogin(existingUser));
   }, [dispatch]);
 
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error.message}</p>}
-      {loginData && <p>Customer = {loginData.email} is logged!</p>}
-    </div>
+    <Container maxWidth="xs" sx={{ mb: 3 }} className="container">
+      <h1>Account login</h1>
+      <LoginForm></LoginForm>
+    </Container>
   );
 }
