97975ba765482eae1462b6f1b02b2f942e191499
Julia Khvatova <jkhvatova@gmail.com>
2023-08-17 01:35:22 +0300

    feat: add login to registration page link (EC-50)

--- ecommerce-app/src/pages/LoginPage.tsx
+++ ecommerce-app/src/pages/LoginPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { LoginForm } from "../components/LoginForm/LoginForm";
+import { Box } from "@mui/material";
 import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
 
 export function LoginPage() {
   // const dispatch = useAppDispatch();
@@ -17,6 +19,11 @@ export function LoginPage() {
     <Container maxWidth="xs" sx={{ mb: 3 }} className="container">
       <h1>Account login</h1>
       <LoginForm></LoginForm>
+      <Box sx={{ pt: "2em" }}>
+        <Link className="redirect-link" to="/registration" color="primary">
+          Not yet a member? Register here!
+        </Link>
+      </Box>
     </Container>
   );
 }
