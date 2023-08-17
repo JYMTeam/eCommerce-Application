42740bde557a8fd71fed7b9057e113729c700fa7
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-17 12:28:54 +0200

    refactor: rename registration to signup (EC-47)

--- ecommerce-app/src/pages/SignupPage.tsx
+++ ecommerce-app/src/pages/SignupPage.tsx
@@ -1,3 +1,15 @@
+import { RegistrationForm } from "../components/SignupForm/SignupForm";
+import Container from "@mui/material/Container";
+
 export function SignupPage() {
-  return <div>Signup</div>;
+  return (
+    <Container
+      maxWidth="md"
+      sx={{ height: 1 / 1, pb: 5 }}
+      className="container"
+    >
+      <h1>Register here</h1>
+      <RegistrationForm></RegistrationForm>
+    </Container>
+  );
 }
