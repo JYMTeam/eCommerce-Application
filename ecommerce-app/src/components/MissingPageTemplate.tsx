6b7fe630c83122db3e655f41eac6b9522f94f0e1
Julia Khvatova <jkhvatova@gmail.com>
2023-08-21 02:54:13 +0300

    refactor: not found and work in progress pages (EC-69)

---
+++ ecommerce-app/src/components/MissingPageTemplate.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Box, Grid, Button } from "@mui/material";
+
+type Data = {
+  title: string;
+  desc: string;
+  img: string;
+};
+export const MissingPageTemplate = ({ title, desc, img }: Data) => {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      height="80vh"
+    >
+      <Typography variant="h3" component="h2" align="center">
+        {title}
+      </Typography>
+      <Typography>{desc}</Typography>
+      <Box display="flex" justifyContent="center" sx={{ width: "100%" }}>
+        <Box
+          component="img"
+          src={img}
+          alt={title}
+          sx={{ height: "auto", maxwidth: "100%" }}
+        />
+      </Box>
+      <Box sx={{ mb: "2em" }}>
+        <Button component={Link} to="/" variant="contained" color="primary">
+          Go to Main page
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
--- ecommerce-app/src/pages/NotFoundPage.tsx
+++ ecommerce-app/src/pages/NotFoundPage.tsx
@@ -1,32 +1,19 @@
 import React from "react";
 import errorGif from "../assets/error-404.gif";
-import { Link } from "react-router-dom";
-
-import { Typography, Box, Grid, Button } from "@mui/material";
+import { MissingPageTemplate } from "../components/MissingPageTemplate";
 export default function NotFoundPage() {
+  const pageData = {
+    title: "Oops!",
+    desc: "We can't find the page you are looking for!",
+    img: errorGif,
+  };
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ my: 20 }}
-    >
-      <Typography variant="h2" component="h2">
-        Oops!
-      </Typography>
-      <Typography>We can't find the page you are looking for!</Typography>
-      <Box
-        component="img"
-        src={errorGif}
-        alt={"404"}
-        sx={{ height: "100%", width: "auto" }}
+    <>
+      <MissingPageTemplate
+        title={pageData.title}
+        desc={pageData.desc}
+        img={pageData.img}
       />
-      <Box sx={{ mb: "4em" }}>
-        <Button component={Link} to="/" variant="contained" color="primary">
-          Go to Main page
-        </Button>
-      </Box>
-    </Grid>
+    </>
   );
 }
