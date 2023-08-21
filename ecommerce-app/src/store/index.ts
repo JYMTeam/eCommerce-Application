beb8b405bbe446122229c57e464cdd6470bcfb3e
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-21 18:40:39 +0200

    fix: rename to RootState (EC-83)

--- ecommerce-app/src/store/index.ts
+++ ecommerce-app/src/store/index.ts
@@ -26,6 +26,6 @@ export function setupStore() {
 }
 
 //оперделяем типы
-export type RootSate = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore["dispatch"];
