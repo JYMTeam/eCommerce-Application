3cd90b9bf3969e069562333f57bef1cf9435a130
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-08-28 16:21:56 +0200

    refactor: delete comments in ShopPage (EC-91)

--- ecommerce-app/src/pages/ShopPage.tsx
+++ ecommerce-app/src/pages/ShopPage.tsx
@@ -4,19 +4,6 @@ import ProductsPagination from "../components/ProductsList/ProductsPagination";
 import ProductsAttributes from "../components/ProductsList/ProductsAttributes";
 
 export function ShopPage() {
-  //check token after loading
-  // const [isFiltered, setFiltered] = useState(false);
-  // const dispatch = useAppDispatch();
-
-  // useEffect(() => {
-  //   const filtration = () => {
-  //     if (!isFiltered) {
-  //       dispatch(fetchFilterProducts());
-  //       setFiltered(true);
-  //     }
-  //   };
-  //   filtration();
-  // }, [dispatch, setFiltered, isFiltered]);
   return (
     <>
       <ProductsAttributes />
