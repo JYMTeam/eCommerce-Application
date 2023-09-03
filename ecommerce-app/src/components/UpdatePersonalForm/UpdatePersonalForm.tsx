5f5c367371a910d872d851bf298d004dd272d093
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-09-03 17:23:20 +0200

    feat: add fetchUpdateUserPersonalInfo (EC-103)

--- ecommerce-app/src/components/UpdatePersonalForm/UpdatePersonalForm.tsx
+++ ecommerce-app/src/components/UpdatePersonalForm/UpdatePersonalForm.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { setUpdatePersonalSchema } from "../../utils/validation-schemas";
-import { useAppSelector } from "../../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { Formik, Form } from "formik";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { initialUpdatePersonalValues } from "../../constants/constants";
-import { Stack } from "@mui/material";
+import { FORM_DATE_FORMAT, MAX_HUMAN_AGE } from "../../constants/constants";
+import { Alert, AlertTitle, Stack } from "@mui/material";
+import { subtractYears } from "../../utils/utils";
+import dayjs from "dayjs";
+import { DatePicker } from "@mui/x-date-pickers";
+import { IUpdatePersonalValues } from "../../types";
+import { fetchUpdateUserPersonalInfo } from "../../store/actions/userLoginActions";
+import { userLoginClearErrorMessage } from "../../store/slices/userLoginSlice";
+import { successMessageHandler } from "../SignupForm/signupHelpers";
 
 export interface UpdatePersonalProps {
   email: string;
@@ -22,8 +29,17 @@ export function UpdatePersonalForm({
   dateOfBirth,
 }: UpdatePersonalProps) {
   const UpdatePersonalSchema = setUpdatePersonalSchema();
-  const { loading } = useAppSelector((state) => state.userLogin);
+  const { loading, errorMessage, loginData, tokenData, isSuccessMessage } =
+    useAppSelector((state) => state.userLogin);
+  const BIRTHDAY_INPUT_NAME = "dateOfBirth";
 
+  const initialUpdatePersonalValues: IUpdatePersonalValues = {
+    firstName: firstName || "",
+    lastName: lastName || "",
+    email: email || "",
+    dateOfBirth: dateOfBirth || "",
+  };
+  const dispatch = useAppDispatch();
   return (
     <Formik
       initialValues={initialUpdatePersonalValues}
@@ -31,26 +47,33 @@ export function UpdatePersonalForm({
       onSubmit={(values) => {
         console.log("save values");
         console.log(values);
+
+        if (loginData && tokenData && tokenData?.token !== "") {
+          dispatch(fetchUpdateUserPersonalInfo(tokenData, loginData, values));
+        }
       }}
     >
       {(formik) => {
-        const { handleChange, errors } = formik;
+        const { handleChange, errors, setFieldValue } = formik;
         const onInputChange = (
           event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
         ) => {
-          // if (errorMessage) {
-          //   dispatch(userLoginClearErrorMessage());
-          // }
+          if (errorMessage) {
+            dispatch(userLoginClearErrorMessage());
+          }
           handleChange(event);
         };
 
         const onInputFocus = () => {
-          // if (errorMessage) {
-          //   dispatch(userLoginClearErrorMessage());
-          // }
+          if (errorMessage) {
+            dispatch(userLoginClearErrorMessage());
+          }
         };
-
-        // const onPasswordCheckChange = () => setShowPassword(!showPassword);
+        const onDatePickerChange = (value: dayjs.Dayjs | null) => {
+          if (!value) return false;
+          const formatedValue = value.format(FORM_DATE_FORMAT);
+          setFieldValue(BIRTHDAY_INPUT_NAME, formatedValue, true);
+        };
         return (
           <Form
             data-testid="update-personal-form"
@@ -60,27 +83,69 @@ export function UpdatePersonalForm({
             <FormControl fullWidth>
               <Stack spacing={2}>
                 <TextField
+                  autoComplete="off"
+                  name="firstName"
+                  label="First name"
+                  required={true}
+                  onChange={onInputChange}
+                  onFocus={onInputFocus}
+                  helperText={errors.firstName}
+                  error={!!errors.firstName}
+                  defaultValue={firstName}
+                />
+                <TextField
+                  autoComplete="off"
+                  name="lastName"
+                  label="Last name"
+                  required={true}
+                  onChange={onInputChange}
+                  onFocus={onInputFocus}
+                  helperText={errors.lastName}
+                  error={!!errors.lastName}
+                  defaultValue={lastName}
+                />
+                <TextField
                   name="email"
                   label="Email"
-                  // variant="standard"
                   placeholder=" user@example.com"
                   required={true}
-                  // sx={{ mb: 1 }}
                   onChange={onInputChange}
                   onFocus={onInputFocus}
                   helperText={errors.email}
                   error={!!errors.email}
                   defaultValue={email}
-                  // disabled={isSuccessMessage}
                 />
-                <Button
-                  type="submit"
-                  // variant="contained"
-                  // size="large"
-                  disabled={loading}
-                >
+                <DatePicker
+                  label="Date of birth"
+                  maxDate={dayjs(new Date())}
+                  minDate={dayjs(subtractYears(new Date(), MAX_HUMAN_AGE))}
+                  orientation="portrait"
+                  onChange={onDatePickerChange}
+                  slotProps={{
+                    textField: {
+                      required: true,
+                      error: !!errors.dateOfBirth,
+                      helperText: errors.dateOfBirth,
+                    },
+                  }}
+                  defaultValue={dayjs(new Date(dateOfBirth || ""))}
+                />
+                <Button type="submit" disabled={loading}>
                   Save Changes
                 </Button>
+                {isSuccessMessage && (
+                  <>
+                    {successMessageHandler(
+                      "You have successfully updated your personal data!",
+                    )}
+                  </>
+                )}
+                {errorMessage && (
+                  <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    {errorMessage}
+                  </Alert>
+                )}
               </Stack>
             </FormControl>
           </Form>
--- ecommerce-app/src/store/actions/userLoginActions.ts
+++ ecommerce-app/src/store/actions/userLoginActions.ts
@@ -14,8 +14,14 @@ import {
 } from "../slices/userLoginSlice";
 import { getApiPassRoot } from "../../commercetools-sdk/builders/ClientBuilderWithPass";
 import { passToken } from "../../commercetools-sdk/PassTokenCache/PassTokenCache";
-import { AuthErrorResponse } from "@commercetools/platform-sdk";
+import {
+  AuthErrorResponse,
+  Customer,
+  MyCustomerUpdate,
+  MyCustomerUpdateAction,
+} from "@commercetools/platform-sdk";
 import { getApiTokenRoot } from "../../commercetools-sdk/builders/ClientBuilderWithExistingToken";
+import { IUpdatePersonalValues } from "../../types";
 
 export const fetchUserLogin = (userAuthOptions: UserAuthOptions) => {
   return async (dispatch: AppDispatch) => {
@@ -62,6 +68,46 @@ export const fetchLoginWithToken = (existingToken: TokenStore) => {
       const body = error.body;
       if (body) {
         dispatch(userLoginReset());
+      }
+    }
+  };
+};
+
+export const fetchUpdateUserPersonalInfo = (
+  existingToken: TokenStore,
+  userCurrentData: Customer,
+  userUpdatedData: IUpdatePersonalValues,
+) => {
+  const { email, firstName, lastName, dateOfBirth } = userUpdatedData;
+
+  const actions: MyCustomerUpdateAction[] = [
+    { action: "changeEmail", email },
+    { action: "setDateOfBirth", dateOfBirth },
+    { action: "setFirstName", firstName },
+    { action: "setLastName", lastName },
+  ];
+  const updateCustomer: MyCustomerUpdate = {
+    version: userCurrentData.version,
+    actions,
+  };
+  return async (dispatch: AppDispatch) => {
+    try {
+      dispatch(userLoginFetching());
+      const answer = await getApiTokenRoot(existingToken.token)
+        .me()
+        .post({
+          body: updateCustomer,
+        })
+        .execute();
+      dispatch(setIsSuccess());
+      // setTimeout(() => {
+      dispatch(userLoginFetchSuccess(answer.body));
+      // }, 1500);
+    } catch (e) {
+      const error = e as ClientResponse<AuthErrorResponse>;
+      const body = error.body;
+      if (body) {
+        dispatch(userLoginFetchError(body));
       }
     }
   };
