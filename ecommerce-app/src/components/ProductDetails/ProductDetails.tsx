e8f8e63a96cde3a07edf221a6bdcebb3990cd16a
Julia Khvatova <jkhvatova@gmail.com>
2023-09-04 12:09:20 +0300

    feat: add discount price on detail page  EC-110

--- ecommerce-app/src/components/ProductDetails/ProductDetails.tsx
+++ ecommerce-app/src/components/ProductDetails/ProductDetails.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { fetchProductDetails } from "../../store/actions/productDetailsActions";
 import { parseProducts } from "../../utils/dataParsers";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography, Chip } from "@mui/material";
 import { IParsedProduct } from "../../types";
 import { ProductCarousel } from "../ProductCarousel/ProductCarousel";
 const MD_COLS = 6;
@@ -11,9 +11,11 @@ const SM_COLS = 12;
 const GRID_SPACING = 2;
 const PRODUCT_TITLE_FONTSIZE = "2em";
 const PRODUCT_DESC_FONTSIZE = "1em";
-const PRODUCT_PRICE_FONTSIZE = "1.4em";
 const PRODUCT_DESC_MARGIN = 3;
 const BUTTON_COLOR = "#F9C152";
+const PRICE_MR = 1;
+const PRICE_BG_COLOR = "rgba(0, 0, 0, 0.08)";
+const DISCOUNT_BG_COLOR = "#00ffbb7d";
 
 export default function ProductDetail() {
   const { id } = useParams();
@@ -42,71 +44,72 @@ export default function ProductDetail() {
   }
   return (
     <div>
-      {parsedProduct.map(({ id, name, longDescription, images, price }) => (
-        <Grid container spacing={GRID_SPACING} key={id}>
-          <Grid
-            item
-            md={MD_COLS}
-            sm={SM_COLS}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <ProductCarousel images={images} />
-          </Grid>
-          <Grid
-            item
-            md={MD_COLS}
-            sm={SM_COLS}
-            sx={{
-              textAlign: {
-                xs: "center",
-                md: "left",
-              },
-            }}
-          >
-            <Typography
-              gutterBottom
-              variant="h3"
-              component="h3"
-              sx={{ fontSize: PRODUCT_TITLE_FONTSIZE }}
-            >
-              {name}
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
+      {parsedProduct.map(
+        ({ id, name, longDescription, images, price, discount }) => (
+          <Grid container spacing={GRID_SPACING} key={id}>
+            <Grid
+              item
+              md={MD_COLS}
+              sm={SM_COLS}
               sx={{
-                mt: PRODUCT_DESC_MARGIN,
-                mb: PRODUCT_DESC_MARGIN,
-                fontSize: PRODUCT_DESC_FONTSIZE,
+                display: "flex",
+                justifyContent: "center",
               }}
             >
-              {longDescription as unknown as string}
-            </Typography>
-            <Typography
-              color="text.secondary"
+              <ProductCarousel images={images} />
+            </Grid>
+            <Grid
+              item
+              md={MD_COLS}
+              sm={SM_COLS}
               sx={{
-                mt: PRODUCT_DESC_MARGIN,
-                mb: PRODUCT_DESC_MARGIN,
-                fontSize: PRODUCT_PRICE_FONTSIZE,
+                textAlign: {
+                  xs: "center",
+                  md: "left",
+                },
               }}
             >
-              {price}
-            </Typography>
-            <Button
-              size="small"
-              sx={{ color: BUTTON_COLOR }}
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              Add to cart
-            </Button>
+              <Typography
+                gutterBottom
+                variant="h3"
+                component="h3"
+                sx={{ fontSize: PRODUCT_TITLE_FONTSIZE }}
+              >
+                {name}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{
+                  mt: PRODUCT_DESC_MARGIN,
+                  mb: PRODUCT_DESC_MARGIN,
+                  fontSize: PRODUCT_DESC_FONTSIZE,
+                }}
+              >
+                {longDescription as unknown as string}
+              </Typography>
+              <Chip
+                label={discount ? discount : price}
+                size="small"
+                sx={{
+                  mr: PRICE_MR,
+                  background: discount ? DISCOUNT_BG_COLOR : PRICE_BG_COLOR,
+                }}
+              />
+              <span className="discount">{discount ? price : discount}</span>
+              <Button
+                size="small"
+                sx={{ color: BUTTON_COLOR }}
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                Add to cart
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      ))}
+        ),
+      )}
     </div>
   );
 }
