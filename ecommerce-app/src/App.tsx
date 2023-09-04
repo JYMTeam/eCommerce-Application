a90c231ed5c1459137179119b9b87fb8cd487c66
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-09-04 17:58:21 +0200

    fix: routes (EC-103)

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -108,7 +108,6 @@ function App() {
     },
     { path: "/shop", element: <ShopPage /> },
     { path: "/shop/:id", element: <ProductDetailsPage /> },
-    { path: "/shop/:id", element: <ProductDetailsPage /> },
     { path: "/categories/:id", element: <CategoryPage /> },
     { path: "/cart", element: <CartPage /> },
     { path: "*", element: <NotFoundPage /> },
