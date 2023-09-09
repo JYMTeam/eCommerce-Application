d9e08a4490e11f22f6d03275b8e62bfaa2c303df
Mariya Demy <Mariya.Demi373@gmail.com>
2023-09-09 18:41:52 +0300

    feat: product skeletons (EC-132)

--- ecommerce-app/src/components/ProductsList/ProductsList.tsx
+++ ecommerce-app/src/components/ProductsList/ProductsList.tsx
@@ -13,17 +13,20 @@ import {
   CardContent,
   CardMedia,
   Chip,
+  Skeleton,
   Typography,
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { Link, useParams } from "react-router-dom";
 import { parseProducts } from "../../utils/dataParsers";
 import { SIDEBAR_WIDTH } from "../ProductsSidebar/ProductsSidebar";
+import { DEFAULT_PRODUCTS_LIMIT } from "../../constants/constants";
+import { IParsedProduct } from "../../types";
 
 const MD_COLS = 4;
 const SM_COLS = 6;
 const XS_COLS = 12;
-const GRID_SPACING = 2;
+export const GRID_SPACING = 2;
 const CARD_MIN_HEIGHT = 320;
 const CARD_MAX_WIDTH = 325;
 const CARD_BOX_SHADOW = 3;
@@ -37,6 +40,8 @@ const PRICE_MR = 1;
 const PRICE_BG_COLOR = "rgba(0, 0, 0, 0.08)";
 const DISCOUNT_BG_COLOR = "#00ffbb7d";
 export const PRODUCT_LIST_PADDING = 3;
+const SKELETON_DESC_HEIGHT = 63;
+const SKELETON_PRICE_HEIGHT = 30;
 
 export default function ProductsList() {
   const { errorMessage, loading, products, page, limit, filterParams } =
@@ -54,9 +59,6 @@ export default function ProductsList() {
     }
   }, [dispatch, offset, filterParams, categoryId]);
 
-  if (loading) {
-    return <p className="notification-message">Loading...</p>;
-  }
   if (errorMessage) {
     return <p className="notification-message">{errorMessage}</p>;
   }
@@ -70,38 +72,53 @@ export default function ProductsList() {
       }}
     >
       <Grid container spacing={GRID_SPACING}>
-        {parsedProducts.map(
-          ({ id, name, description, images, price, discount }) => (
-            <Grid md={MD_COLS} sm={SM_COLS} xs={XS_COLS} key={id}>
-              <Card
+        {(loading
+          ? Array.from(new Array(DEFAULT_PRODUCTS_LIMIT))
+          : parsedProducts
+        ).map((item: IParsedProduct | undefined, index: number) => (
+          <Grid
+            md={MD_COLS}
+            sm={SM_COLS}
+            xs={XS_COLS}
+            key={item ? item.id : index}
+          >
+            <Card
+              sx={{
+                maxWidth: CARD_MAX_WIDTH,
+                boxShadow: CARD_BOX_SHADOW,
+                minHeight: CARD_MIN_HEIGHT,
+                height: CARD_HEIGHT,
+                ":hover": {
+                  boxShadow: 7,
+                },
+              }}
+            >
+              <CardActionArea
+                component={Link}
+                to={`/product/${item ? item.id : ""}`}
                 sx={{
-                  maxWidth: CARD_MAX_WIDTH,
-                  boxShadow: CARD_BOX_SHADOW,
-                  minHeight: CARD_MIN_HEIGHT,
-                  height: CARD_HEIGHT,
-                  ":hover": {
-                    boxShadow: 7,
-                  },
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "flex-start",
+                  alignContent: "space-between",
+                  height: "100%",
                 }}
               >
-                <CardActionArea
-                  component={Link}
-                  to={`/product/${id}`}
-                  sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "flex-start",
-                    alignContent: "space-between",
-                    height: "100%",
-                  }}
-                >
-                  <div>
+                <div style={{ width: "100%" }}>
+                  {item ? (
                     <CardMedia
                       component="img"
-                      alt={name as unknown as string}
+                      alt={item.name as unknown as string}
                       height={CARD_MEDIA_HEIGHT}
-                      image={images[0].url}
+                      image={item.images[0].url}
                     />
+                  ) : (
+                    <Skeleton
+                      variant="rectangular"
+                      height={CARD_MEDIA_HEIGHT}
+                    />
+                  )}
+                  {item ? (
                     <CardContent>
                       <Typography
                         gutterBottom
@@ -109,46 +126,57 @@ export default function ProductsList() {
                         component="h4"
                         sx={{ fontSize: CARD_TITLE_FONTSIZE }}
                       >
-                        {name}
+                        {item.name}
                       </Typography>
                       <Typography
                         variant="body2"
                         color="text.secondary"
                         sx={{ mb: CARD_DESC_MB, fontSize: CARD_DESC_FONTSIZE }}
                       >
-                        {description as unknown as string}
+                        {item.description as unknown as string}
                       </Typography>
                       <Chip
-                        label={discount ? discount : price}
+                        label={item.discount ? item.discount : item.price}
                         size="small"
                         sx={{
                           mr: PRICE_MR,
-                          background: discount
+                          background: item.discount
                             ? DISCOUNT_BG_COLOR
                             : PRICE_BG_COLOR,
                         }}
                       />
                       <span className="discount">
-                        {discount ? price : discount}
+                        {item.discount ? item.price : item.discount}
                       </span>
                     </CardContent>
-                  </div>
-                  <CardActions>
-                    <Button
-                      size="small"
-                      sx={{ color: BUTTON_COLOR }}
-                      onClick={(e) => {
-                        e.preventDefault();
-                      }}
-                    >
-                      Add to cart
-                    </Button>
-                  </CardActions>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ),
-        )}
+                  ) : (
+                    <CardContent>
+                      <Skeleton width="100%" />
+                      <Skeleton
+                        sx={{ mb: CARD_DESC_MB }}
+                        width="100%"
+                        height={SKELETON_DESC_HEIGHT}
+                      />
+                      <Skeleton width="30%" height={SKELETON_PRICE_HEIGHT} />
+                    </CardContent>
+                  )}
+                </div>
+                <CardActions>
+                  <Button
+                    size="small"
+                    sx={{ color: BUTTON_COLOR }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                    }}
+                    disabled={loading}
+                  >
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
