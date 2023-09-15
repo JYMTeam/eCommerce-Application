f41fa9bd995a7e5f4c15939f32956d8755260ac6
Mariya Demy <Mariya.Demi373@gmail.com>
2023-09-16 00:07:08 +0300

    refactor: remove cards border (EC-145)

--- ecommerce-app/src/components/AboutUsStack/AboutUsStack.tsx
+++ ecommerce-app/src/components/AboutUsStack/AboutUsStack.tsx
@@ -6,11 +6,10 @@ import PhotoCardPart from "./PhotoCardPart";
 import ContributionCardPart from "./ContributionCardPart";
 
 const CARD_MAX_WIDTH = 425;
-const BORDER_WIDTH = 2;
 
 export default function AboutUsStack() {
   return (
-    <Stack direction={{ sm: "column" }}>
+    <Stack direction={{ sm: "column" }} spacing={{ sm: 2 }} sx={{ mb: 2 }}>
       {teamMembersInfo.map((teamInfo, index) => {
         return (
           <Card
@@ -18,22 +17,18 @@ export default function AboutUsStack() {
             sx={{
               display: "flex",
               flexDirection: { md: "row", xs: "column" },
-              borderRadius: 0,
-              border: `black 1px solid`,
-              width: {
-                md: `calc(100% - ${BORDER_WIDTH}px)`,
-                xs: CARD_MAX_WIDTH,
-              },
+              boxShadow: 3,
+              width: { md: "100%", xs: CARD_MAX_WIDTH },
             }}
           >
             {index % 2 === 0 ? (
               <>
                 <PhotoCardPart data={teamInfo}></PhotoCardPart>
-                <ContributionCardPart data={teamInfo} index={index} />
+                <ContributionCardPart data={teamInfo} />
               </>
             ) : (
               <>
-                <ContributionCardPart data={teamInfo} index={index} />
+                <ContributionCardPart data={teamInfo} />
                 <PhotoCardPart data={teamInfo}></PhotoCardPart>
               </>
             )}
