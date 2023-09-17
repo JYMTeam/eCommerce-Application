6d2df0424486047421462980a1bc88d8d76d7658
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-09-17 07:23:02 +0200

    fix: empty cart message (EC-141)

--- ecommerce-app/src/components/MissingPageTemplate.tsx
+++ ecommerce-app/src/components/MissingPageTemplate.tsx
@@ -22,18 +22,32 @@ export const MissingPageTemplate = ({
       direction="column"
       justifyContent="center"
       alignItems="center"
-      height="80vh"
     >
-      <Typography variant="h3" component="h2" align="center">
+      <Typography
+        variant="h6"
+        component="h2"
+        sx={{
+          textAlign: "center",
+          textTransform: "uppercase",
+          marginBottom: "0.5em",
+        }}
+      >
         {title}
       </Typography>
-      <Typography>{desc}</Typography>
+      <Typography variant="body2" component="p" align="center">
+        {desc}
+      </Typography>
       <Box display="flex" justifyContent="center" sx={{ width: "100%" }}>
         <Box
           component="img"
           src={img}
           alt={title}
-          sx={{ height: "40vh", maxwidth: "100%", padding: "1.5em 0" }}
+          sx={{
+            height: "40vh",
+            maxWidth: "100%",
+            objectFit: "contain",
+            padding: "1.5em 0",
+          }}
         />
       </Box>
       <Box sx={{ mb: "2em" }}>
